f71af9904eea1a89a271c868687b424257646d8e
Felipe Augusto Azuero Mutis <azuero@protonmail.com>
2019-11-11 14:35:18 +0100

    Clean the project.

--- inequality_research_survey/pages.py
+++ inequality_research_survey/pages.py
@@ -3,21 +3,8 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
-    pass
-
 class DemographicQuestions(Page):
     form_model = 'player'
     form_fields = ['gender', 'age']
-
-
-class ResultsWaitPage(WaitPage):
-    def after_all_players_arrive(self):
-        pass
-
-
-class Results(Page):
-    pass
-
 
 page_sequence = [DemographicQuestions]
